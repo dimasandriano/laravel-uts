f6dc2573877ec6a641d9d9dc019ed565e1182f0b
Bimapras000 <prasbima50@gmail.com>
2022-12-08 09:22:46 +0700

    detail

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -7,12 +7,33 @@
 @section('content')
 
 
-<div class="card mb-3">
+<!-- <div class="card mb-3">
   <img src="{{$jsonfile['gambar']}}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">{{$jsonfile['title']}}</h5>
     <p class="card-text">{{$jsonfile['deskripsi']}}</p>
     <a href="/" class="btn btn-primary">Back</a>
+  </div>
+</div> -->
+
+<div class="container">
+  <div class="row">
+    <div class="col d-flex justify-content-center">
+      <img src="{{$jsonfile['gambar']}}" class="card-img-top w-75" alt="...">
+    </div>
+    <div class="col">
+    <h5 class="card-title-justify">{{$jsonfile['title']}}</h5><br>
+    <div class="card-text-justify">
+    <b>Penulis : {{$jsonfile['author']}}</b><br>
+    <b>Genre : {{$jsonfile['genre']}}</b><br>
+    <b>Tahun : {{$jsonfile['tahun']}}</b><br><br>
+    <hr/>
+    <p><p>{{$jsonfile['deskripsi']}}</p></p><br>
+    </div>
+    <div class="d-flex justify-content-end">
+      <a href="/" class="btn btn-primary">Back</a>
+    </div>
+    </div>
   </div>
 </div>
 
